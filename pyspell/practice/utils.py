3bd180a32c2232bd43eec8ad11fc325129032af7
Igor Malkov <imalkov@paypal.com>
2022-12-29 09:01:38 +0200

    introduce audio context manager

--- pyspell/practice/utils.py
+++ pyspell/practice/utils.py
@@ -5,16 +5,29 @@ import os
 import time
 from typing import List, Union
 from pyspell.practice.models import Word
+import contextlib
+
+@contextlib.contextmanager
+def audio(blob):
+    f = tempfile.NamedTemporaryFile(mode="w+b", suffix=".mp3", delete=False)                
+    f.write(blob)
+    fname = f.name
+    f.close()
+    
+    yield fname
+    
+    os.remove(fname)
+    
 
 def practice_on(words: List[Word]):
     def wrapper(indexes: List[int]) -> Union[float, bool, List[int]]:
-        click.clear()
         
         incorrect_indexes = []
         practice_force_complete = False
         cnt_correct_words = 0.0
-        
         random_indexes = random.sample(indexes, len(indexes))
+        
+        click.clear()
         with click.progressbar(random_indexes, color="bright_white") as bar:
             for n, i in enumerate(bar):
                 if practice_force_complete: # handle .q command
@@ -23,37 +36,31 @@ def practice_on(words: List[Word]):
                     break
                 
                 word = words[i]
-                name, desc, audio = word.word
-                f = tempfile.NamedTemporaryFile(mode="w+b", suffix=".mp3", delete=False)                
-                f.write(audio)
-                fname = f.name
-                f.close()
+                name, desc, audio_blob = word.word
                 
-                while True:
-                    # click.clear()
-                    click.echo(click.style(f"\n\nDESCRIPTION: \n\t{desc}", fg="bright_white"))
-                    os.system("afplay " + fname)
-                    answer = str.lower(str.strip(click.prompt(f"\nENTER YOUR ANSER")))
-                    # hanlde command
-                    if answer == ".r":
-                        continue
-                    elif answer == ".q":
-                        practice_force_complete = True
-                        break
-                    
-                    word.answer = answer
-                    
-                    if name == answer:
-                        cnt_correct_words+=1
-                        click.echo(click.style(f"\n {answer.upper()} = {name.upper()}", fg="green"))
-                        time.sleep(1)
-                    else:
-                        click.echo(click.style(f"\n {answer.upper()} ≠ {name.upper()}", fg="red"))                
-                        incorrect_indexes.append(i)
-                        time.sleep(5)
-                    break
-                
-                os.remove(fname)
+                with audio(audio_blob) as fname:
+                    while True:
+                        click.echo(click.style(f"\n\nDESCRIPTION: \n\t{desc}", fg="bright_white"))
+                        os.system("afplay " + fname)
+                        answer = str.lower(str.strip(click.prompt(f"\nENTER YOUR ANSER")))
+                        # hanlde command
+                        if answer == ".r":
+                            continue
+                        elif answer == ".q":
+                            practice_force_complete = True
+                            break
+                        
+                        word.answer = answer
+                        
+                        if name == answer:
+                            cnt_correct_words+=1
+                            click.echo(click.style(f"\n {answer.upper()} = {name.upper()}", fg="green"))
+                            time.sleep(1)
+                        else:
+                            click.echo(click.style(f"\n {answer.upper()} ≠ {name.upper()}", fg="red"))                
+                            incorrect_indexes.append(i)
+                            time.sleep(5)
+                        break                
                 click.clear()
         return cnt_correct_words, practice_force_complete, sorted(incorrect_indexes)
     return wrapper
